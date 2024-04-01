6dc115187201bd489f829ce1d29fc2546066256a
Bobie Brighton <111507754+user-bob@users.noreply.github.com>
2024-04-01 02:40:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def main(
 
 if __name__ == "__main__":
     browsers_list = list(browsers.browsers())
-    skip_browsers = ["msie", "safari", 'chromium', 'opera', 'msedge']
+    skip_browsers = ["msie", "safari", 'chromium', 'opera', 'msedge', 'chrome']
     for browser in browsers_list:
         browser_type = browser['browser_type']
         if browser_type in skip_browsers:
